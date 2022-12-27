00d2da5d52785796a654f256c686bace39f8a47b
harrywalkr <116752427+harrywalkr@users.noreply.github.com>
2022-12-27 22:35:02 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// app.js
+import { useMetaMask } from "metamask-react";
+
+function App() {
+    const { status, connect, account, chainId, ethereum } = useMetaMask();
+
+    if (status === "initializing") return <div>Synchronisation with MetaMask ongoing...</div>
+
+    if (status === "unavailable") return <div>MetaMask not available :(</div>
+
+    if (status === "notConnected") return <button onClick={connect}>Connect to MetaMask</button>
+
+    if (status === "connecting") return <div>Connecting...</div>
+
+    if (status === "connected") return <div>Connected account {account} on chain ID {chainId}</div>
+
+    return null;
+}
